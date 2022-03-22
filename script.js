ab7b55a85b58b06ac1ee8e5e70fa485674e5930d
RossT <rthomson412@gmail.com>
2022-03-22 10:22:26 -0400

    added counter/ added q&a iteration

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var codeQuiz = [
         question: '',
         a: '',
         b: '',
-        c: '9',
+        c: '',
         d: '',
         answer: ''
     }
@@ -49,3 +49,79 @@ var scoreEl = document.querySelector(".score");
 var questionCounter = 0;
 var currentScore = 99;
 var highScores = [];
+
+// starts countdown/ score from 100
+var scoreCounter = function() {
+    scoreEl.textContent = "Current score: 100"
+
+    var scoreInterval = setInterval(function() {
+        if (currentScore > 0 && questionCounter < codeQuiz.length) {
+            scoreEl.textContent = "Current score: " + currentScore;
+            currentScore--
+        }
+        else {
+            clearInterval(scoreInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
+
+var createQuiz = function() {
+    document.querySelector("#instructions").remove();
+    quizEl.classList.remove("hide")
+
+    nextQues(questionCounter);
+    scoreCounter();
+}
+
+// iterates through questions and answers
+var nextQues = function(index) {
+    var questionHeader = document.querySelector(".question-header");
+    var questionEl = document.querySelector(".question");
+    var btnA = document.getElementById("btn-a");
+    var btnB = document.getElementById("btn-b");
+    var btnC = document.getElementById("btn-c");
+    var btnD = document.getElementById("btn-d");
+
+    questionHeader.textContent = "Question #" + parseInt(index + 1)
+    questionEl.textContent = codeQuiz[index].question;
+    btnA.textContent = codeQuiz[index].a;
+    btnB.textContent = codeQuiz[index].b;
+    btnC.textContent = codeQuiz[index].c;
+    btnD.textContent = codeQuiz[index].d;
+
+    btnA.addEventListener("click", checkAnswer);
+    btnB.addEventListener("click", checkAnswer);
+    btnC.addEventListener("click", checkAnswer);
+    btnD.addEventListener("click", checkAnswer);
+}
+
+var checkAnswer = function(event) {
+    var clickedBtn = event.target.getAttribute("value");
+    var feedbackEl = document.querySelector(".feedback");
+    feedbackEl.classList.remove("hide");
+    
+    // checks button value against correct answer in array
+    if (clickedBtn === codeQuiz[questionCounter].answer) {
+        background.className = "correct";
+        feedbackEl.textContent = "CORRECT!"
+    }
+    else {
+        if (currentScore >= 5) {
+            currentScore -= 5;
+            scoreEl.textContent = "Current score: " + currentScore;
+            }
+        background.className = "incorrect";
+        feedbackEl.classList.remove("hide");
+        feedbackEl.textContent = "INCORRECT!"
+    }
+
+    questionCounter++
+
+    if (questionCounter < codeQuiz.length) {        
+        nextQues(questionCounter);
+    }
+    else {
+        endQuiz();
+    }
+}
